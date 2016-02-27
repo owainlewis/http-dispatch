7b557406eb0224bae63af5db609367500e345c99
Owain Lewis <owain@owainlewis.com>
2016-02-27 12:26:21 +0000

    starting point

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 A high level Haskell HTTP client with a friendly and consistent API.
 
 This library builds upon the http-client library
+
+## Getting Started
+
+```haskell
+import Network.HTTP.Dispatch(runRequest, simpleGET)
+
+let response = runRequest $ simpleGET "http://owainlewis.com"
+
+```
--- src/Network/HTTP/Dispatch/Core.hs
+++ src/Network/HTTP/Dispatch/Core.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Network.HTTP.Dispatch.Core where
 
-import           Network.HTTP.Client as Client
+import           Data.ByteString.Lazy as LBS
+import           Network.HTTP.Client  as Client
 
 data HTTPMethod =
     GET
@@ -10,7 +12,22 @@ data HTTPMethod =
   | DELETE
 
 data HTTPRequest = HTTPRequest {
-    url    :: String
-  , method :: HTTPMethod
+    _url    :: String
+  , _method :: HTTPMethod
 }
+
+simpleGET url = HTTPRequest url GET
+
+toRequest :: HTTPRequest -> IO Client.Request
+toRequest httpRequest = do
+    initReq <- parseUrl (_url httpRequest)
+    let req = initReq { method = "GET" }
+    return req
+
+runRequest :: HTTPRequest -> IO (Response LBS.ByteString)
+runRequest httpRequest = do
+    manager <- newManager defaultManagerSettings
+    request <- toRequest httpRequest
+    response <- httpLbs request manager
+    return response
 
