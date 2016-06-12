11c5200f2ef8bb8233c8b53cdcb41c8a95edd012
Owain Lewis <owain@owainlewis.com>
2016-06-12 08:02:15 +0100

    adds http basic auth test

---
+++ test/Network/HTTP/Dispatch/HeadersSpec.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Network.HTTP.Dispatch.HeadersSpec where
+
+import           Network.HTTP.Dispatch.Headers
+import           Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+    describe "Constructing basic auth" $ do
+        it "Constructs the correct header" $ do
+            let actual = basicAuth "foo" "bar"
+                expected = ("Authorization", "Basic: Zm9vOmJhcg==")
+            actual `shouldBe` expected
