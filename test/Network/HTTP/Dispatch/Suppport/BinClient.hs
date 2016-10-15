96cef324d957b6abf76b0437e2f3b933882b52d9
Owain Lewis <owain@owainlewis.com>
2016-10-15 09:35:42 +0100

    Update BinClient.hs

--- test/Network/HTTP/Dispatch/Suppport/BinClient.hs
+++ test/Network/HTTP/Dispatch/Suppport/BinClient.hs
@@ -3,8 +3,8 @@ module Network.HTTP.Dispatch.Support.BinClient where
 import           Data.Aeson
 import           Network.HTTP.Dispatch.Core
 
--- Utility client for testing against httpbin.org
-
+-- | Utility client for testing against httpbin.org
+--
 url:: String
 url = "https://httpbin.org"
 
