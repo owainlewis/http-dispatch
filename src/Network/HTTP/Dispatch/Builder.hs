01a0e9631a3bbc5da49da9d8e5f8fe8d48517f7d
Owain Lewis <owain@owainlewis.com>
2016-10-19 20:05:28 +0100

    Notes for builder

--- src/Network/HTTP/Dispatch/Builder.hs
+++ src/Network/HTTP/Dispatch/Builder.hs
@@ -1,3 +1,10 @@
 module Network.HTTP.Dispatch.Builder where
 
--- | Namespace for constructing requests
+-- | Namespace for constructing requests. Just sugar for constructing types
+
+-- get url headers = HTTPRequest GET url [] Nothing
+-- post url headers body
+-- put url headers body
+-- patch url headers body
+-- options url headers body
+-- delete url headers body
