95c394998a8460c082d4636224661ae3b9bffea3
Owain Lewis <owain@owainlewis.com>
2016-10-26 07:54:14 +0100

    Update IntegrationSpec.hs

--- test/Network/HTTP/Dispatch/IntegrationSpec.hs
+++ test/Network/HTTP/Dispatch/IntegrationSpec.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Network.HTTP.Dispatch.IntegrationSpec where
 
-import           Network.HTTP.Dispatch.Core
+import           Network.HTTP.Dispatch.Api
 import           Test.Hspec
 
 main :: IO ()
@@ -11,13 +11,13 @@ spec :: Spec
 spec = do
     describe "making GET requests" $ do
         it "returns HTTP 200" $ do
-            response <- runRequest $ getRequest "https://httpbin.org/get" []
+            response <- get "https://httpbin.org/get" []
             (respStatus response) `shouldBe` 200
     describe "making POST requests" $ do
         it "returns HTTP 200" $ do
-            response <- runRequest $ postRequest "https://httpbin.org/post" [] (Just "Hello, World!")
+            response <- post "https://httpbin.org/post" [] (Just "Hello, World!")
             (respStatus response) `shouldBe` 200
     describe "making POST requests" $ do
         it "returns HTTP 200" $ do
-            response <- runRequest $ deleteRequest "https://httpbin.org/delete" []
+            response <- delete "https://httpbin.org/delete" []
             (respStatus response) `shouldBe` 200
